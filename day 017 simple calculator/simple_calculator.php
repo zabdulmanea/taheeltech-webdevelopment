14c5aa38e7211c8599d0471625dd65ab7b8d215a
Zainab Abdulmanea <zabdulmani@stu.kau.edu.sa>
2022-07-21 15:29:02 +0300

    initialize day17 exercise

---
+++ day 017 simple calculator/simple_calculator.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>آلة حاسبة مُبسطة Simple Calculator</title>
+</head>
+<body>
+    <h1>آلة حاسبة مُبسطة Simple Calculator</h1>
+
+    
+</body>
+</html>
