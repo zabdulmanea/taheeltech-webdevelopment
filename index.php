3b07ac5869856cc0dd17e14a72def624c44371f1
Zainab Abdulmanea <zabdulmani@stu.kau.edu.sa>
2022-07-21 15:24:00 +0300

    Added index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="static/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&family=Spline+Sans+Mono:wght@600&family=Tajawal&display=swap" rel="stylesheet">
+    <title> التدريبات العملية للمستوى الأولى</title>
+</head>
+
+<body>
+    <h1>التطبيق العملي لتدريبات المستوى الأول</h1>
+    <div style="width: 100%; height: 8px; border-bottom: 1px solid black; text-align: center; margin-bottom: 50px;">
+        <span style="font-size: 15px; background-color: #fcf8f3; padding: 0 10px;">
+            زينب عبدالمانع
+        </span>
+    </div>
+
+    <div class="exercise_div">
+        <h2 class="ex_no">تطبيق اليوم ٢</h2>
+        <a class="ex_link" target="_blank" href="day 002 - create a simple article/simple article.html" alt="تصميم مقال بسيط">تصميم مقال بسيط</a>
+    </div>
+    <div class="exercise_div">
+        <h2 class="ex_no">تطبيق اليوم ٣</h2>
+        <a class="ex_link" target="_blank" href="day 003 - students grades table/grades table.html" alt="تصميم جدول درجات الطلاب ">تصميم جدول لنتيجة درجات مجموعة طلاب في المواد الدراسية</a>
+    </div>
+    <div class="exercise_div">
+        <h2 class="ex_no">تطبيق اليوم ٥</h2>
+        <a class="ex_link" target="_blank" href="day 005 - new registration form/registration_form.html" alt="تصميم نموذج عضوية جديدة">تصميم نموذج "تسجيل عضوية جديدة"</a>
+    </div>
+    <div class="exercise_div">
+        <h2 class="ex_no">تطبيق اليوم ١١</h2>
+        <a class="ex_link" target="_blank" href="day 011 - draw triangles with stars/draw_triangles.php" alt="رسم مثلثات بالنجوم">رسم مثلثات بالنجوم</a>
+    </div>
+    <div class="exercise_div">
+        <h2 class="ex_no">تطبيق اليوم ١٢</h2>
+        <a class="ex_link" target="_blank" href="day 012 - render chess board/chess_board.php" alt="رسم مخطط قطع الشطرنج">رسم مخطط قطع الشطرنج</a>
+    </div>
+    <div class="exercise_div">
+        <h2 class="ex_no">تطبيق اليوم ١٢</h2>
+        <a class="ex_link" target="_blank" href="day 012 - pyramid multiplication table /pyramid_multiplicatio.php" alt="رسم جدول الضرب">رسم جدول الضرب على شكل مثلث</a>
+    </div>
+    <div class="exercise_div">
+        <h2 class="ex_no">تطبيق اليوم ١٣</h2>
+        <a class="ex_link" target="_blank" href="day 013 -ideal weight/bmi_ibw_calculator.php" alt="حساب كتلة الجسم والوزن المثالي">حساب كتلة الجسم والوزن المثالي</a>
+    </div>
+    <div class="exercise_div">
+        <h2 class="ex_no">تطبيق اليوم ١٦</h2>
+        <a class="ex_link" target="_blank" href="day 016 - students grades/exams results.php" alt="عرض درجات الاختبار للطلاب">عرض درجات الاختبار بالمواد الدراسية لمجموعة طلاب بالصف الأول الثانوي وتحديد النسبة المئوية وحالة الطالب</a>
+    </div>
+
+    <div class="exercise_div">
+        <h2 class="ex_no">تطبيق اليوم ١٧</h2>
+        <a class="ex_link" target="_blank" href="day 017 simple calculator/simple_calculator.php" alt="آلة حاسبة مبسطة">برمجة آلة حاسبة مُبسطة Simple Calculator</a>
+    </div>
+
+    <div style="display: none;">
+    </div>
+
+    <?php include 'footer.php'; ?>
+</body>
+
+</html>
