3a743b67af247820033ed46f18889bc7049d5cbe
Zainab Abdulmanea <zabdulmani@stu.kau.edu.sa>
2022-07-21 15:24:31 +0300

    Added Footer page

---
+++ footer.php
@@ -0,0 +1,30 @@
+<style>
+    /***  footer style
+--------------------------------------------------------------------------- ***/
+
+    footer {
+        width: 100%;
+    }
+
+    footer p {
+        color: #959072;
+        text-align: center;
+        font-size: 0.9em;
+        margin: 0px;
+        margin-bottom: 10px;
+    }
+
+    .heart {
+        display: inline-block;
+        position: relative;
+        color: #b63434;
+    }
+</style>
+
+<footer>
+    <hr>
+    <p>
+        Made with <span class="heart pulse">♥</span>
+        by Zinab Abdulmanea © 2022
+    </p>
+</footer>
