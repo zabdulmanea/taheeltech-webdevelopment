dc86d08289c6c5ca220a913964bebd66aea33942
Zainab Abdulmanea <zabdulmani@stu.kau.edu.sa>
2022-07-27 09:09:22 +0300

    added mb function to test on server

--- day 024 - reverse text/reverse_text.php
+++ day 024 - reverse text/reverse_text.php
@@ -2,6 +2,19 @@
 
 function revstr_char($str)
 {
+    $temp= mb_detect_encoding($str);
+    // if (mb_detect_encoding($str) != 'UTF-8') {
+    //     #built-in function doesn't support UTF-8
+    //     return strrev($str);
+    // } else {
+    //     #for UTF-8
+    //     $len = strlen($str);
+    //     $rev_str = '';
+
+    //     for ($i = $len - 1; $i >= 0; $i--)
+    //         $rev_str .= mb_substr($str, $i, 1);
+    //     return $rev_str;
+    // }
     return 'Success';
 }
 
