7a5804ab99a1b72b2899a4846570b9dae1a23108
Zainab Abdulmanea <zabdulmani@stu.kau.edu.sa>
2022-08-05 16:57:33 +0300

    Create Navigation bar

---
+++ navigation.php
@@ -0,0 +1,7 @@
+<div class="nav-div">
+    <ul class="nav-bar">
+        <li><a class="active" href="index.php">الصفحة الرئيسية</a></li>
+        <li><a href="#news">التدريب التالي</a></li>
+        <li><a href="#contact">التدريب السابق</a></li>
+    </ul>
+</div>
